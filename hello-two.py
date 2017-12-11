25b22b3a36c87922e641243dc63a198e9bf071b1
minbo <hemin_it@163.com>
2017-12-11 22:33:26 +0800

    practice

---
+++ hello-two.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print('hello,world, again')
+print('this is','my','second demo of python')
+print(100)
+print(100+200)
+print('100+200')
+# 注释
+#name = input('please enter your name: ')
+#print('hello', name)
+
+a = 100
+if a >=1000:
+    print(a)
+else:
+    print(-1)
+
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+print(L[0][0])
+print(L[1][1])
+print(L[2][2])
