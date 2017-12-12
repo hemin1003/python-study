ab4605b7d2c652fe579b8d2cd17eabd980b97001
minbo <hemin_it@163.com>
2017-12-12 08:29:35 +0800

    practice

--- hello-two.py
+++ hello-two.py
@@ -11,8 +11,10 @@ print('100+200')
 #print('hello', name)
 
 a = 100
-if a >=1000:
+if a >= 1000:
     print(a)
+elif a == 100:
+    print(250)
 else:
     print(-1)
 
@@ -24,3 +26,36 @@ L = [
 print(L[0][0])
 print(L[1][1])
 print(L[2][2])
+
+#s = input('birth：')
+#birth = int(s)
+#if birth>2000:
+#    print('00后')
+#else:
+#    print('00前')
+
+names = ['Michelle','Bob','Tracy']
+for name in names:
+    print(name)
+
+sum = 0
+for x in range(101):
+    sum = sum + x
+print(sum)
+
+sum = 0
+n = 99
+while n >0 :
+    sum = sum + n
+    n = n - 2
+print(sum)
+
+
+# 请利用循环依次对list中的每个名字打印出Hello, xxx!
+# L = ['Bart', 'Lisa', 'Adam']
+L = ['Bart', 'Lisa', 'Adam']
+for s in L:
+    print('hello', s)
+
+
+
