c81397b7e114aa933aa17c9633395ea3c2dcf7fe
minbo <hemin_it@163.com>
2017-12-14 00:27:14 +0800

    practice

---
+++ function/slice_demo.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print('abc何敏')
+
+L=['a','b','c','d','e']
+print('取前三位=')
+print('每隔两位取一个=')
+print('取最后一个', L[-1:])
+print('取第一个')
+
+
+L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
+
+print('L[0:3] =', L[0:3])
+print('L[:3] =', L[:3])
+print('L[1:3] =', L[1:3])
+print('L[-2:] =', L[-2:])
+
+R = list(range(100))
+print('R[:10] =', R[:10])
+print('R[-10:] =', R[-10:])
+print('R[10:20] =', R[10:20])
+print('R[:10:2] =', R[:10:2])
+print('R[::5] =', R[::5])
---
+++ function/test.py
@@ -0,0 +1,5 @@
+def sumall(x,y):
+    if x+y==2:
+        return 100
+    else:
+        return 0;
