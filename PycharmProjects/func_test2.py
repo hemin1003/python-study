84202040d588db74c2074f936daf246456737826
minbo <hemin_it@163.com>
2018-06-10 04:53:59 +0800

    lambda用法

--- PycharmProjects/func_test2.py
+++ PycharmProjects/func_test2.py
@@ -4,18 +4,18 @@
 # it = iter(list1)
 # print(next(it))
 # print(next(it))
-# print(next(it))
+# print((nextit))
 # print(next(it)) # except
 
 # 迭代器
-for i in range(10, 20, 2):
-    print(i)
+# for i in range(10, 20, 2):
+#     print(i)
 
-# yield关键字，生成器，每次都记录当前位置
+# yield关键字，生成器，每次都记录当前位置返回
 def frange(start, stop, step):
     x = start
     while x < stop:
-        yield x
+        yield float(x)
         x += step
 
 for i in frange(10, 20, 0.5):
---
+++ PycharmProjects/lambda.py
@@ -0,0 +1,13 @@
+# lambda用法
+
+# 普通函数用法
+def sum(a,b):
+    return a+b
+
+print(sum(1,2))
+
+# lambda用法
+# lambda x,y: x+y
+
+# lambda组成结构
+# lambda关键字 参数(可省略): 返回结果
